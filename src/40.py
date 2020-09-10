129ae6edb28bbe0b5900d0305c63eaf83ff30c3b
xiaofeng <1506010130@hhu.edu.cn>
2020-09-10 09:38:31 +0800

    update 40.py

--- src/40.py
+++ src/40.py
@@ -1,7 +1,7 @@
 class Solution:
     '''
     时间巨长：击败5.01%
-    内存还好: 击败78.31% 
+    内存还好: 击败80.94% 
     '''
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         result = list(list())
@@ -11,11 +11,11 @@ class Solution:
         return result
     
     def dfs(self, _candidates, _nowTarget, _nowSolu, _soluList):
-        tempCan = _candidates[:]
-        tempSolu = _nowSolu[:]
-        if _nowTarget < 0:
+        tempCan = _candidates[:]  #复制，保留本层递归的解集
+        tempSolu = _nowSolu[:]  #同理
+        if _nowTarget < 0:  #小于0，已不能找到合适的解集，返回
             return
-        if _nowTarget == 0:
+        if _nowTarget == 0: #等于0，已经找到解集，若其与已有解集不重复，则加入结果
             sortSolu = sorted(tempSolu)
             if sortSolu not in _soluList:
                 _soluList.append(sortSolu)
@@ -23,7 +23,7 @@ class Solution:
         else:
             for i in range(len(_candidates)):
                 tempSolu.append(_candidates[i])
-                tempCan.remove(_candidates[i])
+                tempCan.remove(_candidates[i]) #若使用某元素，则将其从待选集中移除
                 self.dfs(tempCan, _nowTarget - _candidates[i], tempSolu, _soluList)
-                tempSolu.pop()
+                tempSolu.pop() #若不使用某元素，则将其加入到待选集中
                 tempCan.append(_candidates[i])
