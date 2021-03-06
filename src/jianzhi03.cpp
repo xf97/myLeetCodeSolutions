9a9df784bbc9b8e39d3e25f70644fbf1bad3da43
哈利肖不是蛤蜊小 <harleyxiao@foxmail.com>
2021-03-06 12:27:11 +0800

    Update jianzhi03.cpp

--- src/jianzhi03.cpp
+++ src/jianzhi03.cpp
@@ -7,24 +7,31 @@ public:
         //一次遍历，解决问题
         //要用到下标
         /*
-        time defeat: 80.02%
-        space defeat: 83.4%
+        time defeat: 88.29%
+        space defeat: 99.06%
         */
         int temp = 0;   //暂存变量
         for(int i = 0; i < nums.size(); i++){
             //关键在于如果没有重复的话，下标i上的数字就是i
-            while(nums[i] != i){
+            if(nums[i] == i){
+                continue;
+            }
+            else{
                 if(nums[i] == nums[nums[i]]){
                     //在属于他的位置上已经放着他了，那么重复
                     return nums[i];
                 }
                 else{
-                    //否则，把他放到他的位置去
-                    temp = nums[i];
-                    nums[i] = nums[temp];
-                    nums[temp] = temp; 
+                    while(nums[i] != i){
+                        //否则，把他放到他的位置去
+                        //很奇怪，这里nums[temp]如何改成nums[i]的话会超时
+                        //应该是重复修改吗
+                        temp = nums[i];
+                        nums[i] = nums[temp];
+                        nums[temp] = temp;
                 }
             }
+        }
         }
         return 0;
     }
