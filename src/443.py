e69f47303fc9d0762833443e825dda3fe26be0e0
Xiao Harley <harleyxiao@foxmail.com>
2021-07-30 12:02:40 +0800

    Create 443.py

---
+++ src/443.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    '''
+    今早累了，做不出来题
+    烦了，就这样吧
+    '''
+    def compress(self, chars):
+        anchor = write = 0
+        for read, c in enumerate(chars):
+            if read + 1 == len(chars) or chars[read + 1] != c:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
