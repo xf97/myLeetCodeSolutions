4114dcc07c2fb21893d077352cc340cb2594950e
Xiao Harley <harleyxiao@foxmail.com>
2021-09-18 16:37:55 +0800

    Update 530.cpp

--- src/530.cpp
+++ src/530.cpp
@@ -7,13 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 class Solution {
-    /*
-    1.根据二叉搜索树的性质，最小绝对值应该出现在同一棵子树中--错误
-    深度遍历，求值
-    2. 不错，补充，最小绝对值还可以出现在两个地方，左子树的最右叶子和根节点，右子树的最左节点和根节点--错误
-    3. 最土方法，遍历，获得一个新节点，就计算一次新的最小绝对值
-    */
     //哇，新知识，搜索二叉树中序遍历是有序的递增数组
     //time consuming: less than 1 hour
 public:
@@ -53,5 +48,50 @@ public:
             getMinDistance(_node->right, _minD, _valList);
             return;
         }
+    }
+};
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorderTravel(TreeNode * node, vector<int> & val){
+        if(node == nullptr){
+            return;
+        }
+        //左根右
+        inorderTravel(node->left, val);
+        val.push_back(node->val);
+        inorderTravel(node->right, val);
+        return;
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        //中序遍历
+        //边界情况
+        //十分钟，一次提交，时间击败-97.52%，空间击败-14.13%
+        if(root == nullptr){
+            return -1;
+        }
+        vector<int> val;
+        inorderTravel(root, val);
+        //然后计算绝对值
+        int result = INT_MAX;
+        int length = val.size();
+        for(int i = 0; i < length - 1; ++ i){
+            if(abs(val[i] - val[i + 1]) < result){
+                result = abs(val[i] - val[i + 1]);
+            }
+        }
+        return result;
     }
 };
