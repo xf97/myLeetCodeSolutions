ce71b2486f2cdfd44ffc261a88a750876ab0dcba
Xiao Harley <harleyxiao@foxmail.com>
2021-10-11 11:05:52 +0800

    Update 151.py

--- src/151.py
+++ src/151.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def reverseWords(self, s: str) -> str:
         #干翻蚂蚁链
@@ -13,3 +14,10 @@ class Solution:
             else:
                 continue
         return " ".join(result)
+ '''
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        #先整体翻转，再逐个翻转
+        #很棒，一行代码的体验
+        #一分钟，时间击败-40.22%，空间击败-5.17%
+        return " ".join([i[::-1] for i in s[::-1].strip().split()])
